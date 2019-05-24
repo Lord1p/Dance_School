2e49522d04b9ce0be52313031cfc8e1e8e80b62c
Andrew <andrew.ryban@gmail.com>
2019-05-25 02:52:16 +0300

    new frontend features and bug fixes

--- app/src/controllers/courses.controller.js
+++ app/src/controllers/courses.controller.js
@@ -1,15 +1,22 @@
 (function() {
-    "use strict";
-  
-    angular
-      .module("main")
-      .controller("CoursesController", CoursesController);
-  
-    function CoursesController($scope) {
-      init();
-  
-      function init() {
-      }
+  "use strict";
+
+  angular
+    .module("main")
+    .controller("CoursesController", CoursesController);
+
+    CoursesController.$inject = ['$scope', '$http'];
+  function CoursesController($scope, $http) {
+    $scope.courses = [];
+    $scope.empty = "Пока ничего нового :(";
+    init();
+
+    function init() {
+      $http.get('./server/get-client-courses.php')
+        .then(res => {
+          console.log(res.data.clCourses);
+          $scope.courses = res.data.clCourses;
+        });
     }
-  })();
-  
+  }
+})();
--- app/src/controllers/sign-up.controller.js
+++ app/src/controllers/sign-up.controller.js
@@ -5,36 +5,45 @@
     .module("main")
     .controller("SignUpController", SignUpController);
 
-  SignUpController.$inject = ["$rootScope", "$scope", "$http"];
-  function SignUpController($rootScope, $scope, $http) {
+  SignUpController.$inject = ["$rootScope", "$scope", "$http", "encryptor"];
+  function SignUpController($rootScope, $scope, $http, encryptor) {
     $scope.user = {
       firstName: "",
       lastName: "",
       email: "",
       password: "",
       tellNumber: "",
-      mailSending: true,
+      mailSending: true
     };
     $scope.registration = registration;
+    $scope.isOk = true;
 
     init();
 
     function init() {}
 
     function registration() {
+      if ($scope.user.firstName.length == 0) {
+        
+      }
       let preparedData = {
-        name: $scope.user.firstName + ' ' + $scope.user.lastName,
+        name: $scope.user.firstName + " " + $scope.user.lastName,
         email: $scope.user.email,
         tellNumber: $scope.user.tellNumber,
-        password: $scope.user.password,
-        mailSending: $scope.user.mailSending,
-      }
-      $http.post('./server/post-registration.php', preparedData)
-        .then(res => {
-            console.log(res.data);
-            $rootScope.isAuthorizated = true;
-            $rootScope.currentUser = res.data.client;
-        });
+        password: encryptor.enctypt("abcdefghijclmnopqrstwxyz6157480932" ,$scope.user.password),
+        mailSending: $scope.user.mailSending
+      };
+      $http.post("./server/post-registration.php", preparedData).then(res => {
+        console.log(res.data);
+        if (res.data.email) {
+          $rootScope.isAuthorizated = true;
+          $rootScope.currentUser = res.data;
+          $rootScope.currentUser.type = $rootScope.userType.client;
+        }
+        else {
+          $scope.isOk = false;
+        }
+      });
     }
   }
 })();
