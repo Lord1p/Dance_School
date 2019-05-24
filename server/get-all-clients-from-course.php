15f1b6bc36ebfb2202580aead042123f2d3bc246
Lord1p <olehonator@gmail.com>
2019-05-24 23:28:43 +0300

    Add all get request for web application

---
+++ server/get-all-clients-from-course.php
@@ -0,0 +1,43 @@
+<?php
+    include("connect.php");
+    $id = $_REQUEST['id'];
+
+   
+    $projects = $dbh->prepare("SELECT 
+    orders.orderId,
+    orders.code,
+    orders.courseId,
+    clients.name,
+    clients.email,
+    clients.tellNumber,
+    clients.clientId,
+    clients.avatarLink,
+    clients.mailSending
+    FROM
+    orders,clients
+    WHERE
+    orders.courseId = :id and
+    orders.clientId = clients.clientId
+    ");
+    $projects->bindValue('id',$id);
+    $projects->execute();
+    $JSONres=array();
+
+    while($row = $projects->fetch(PDO::FETCH_ASSOC)){
+
+    $res = new R();
+    $res->orderId = $row['orders.orderId'];
+    $res->code = $row['orders.code'];
+    $res->courseId = $row['orders.courseId'];
+    $res->name = $row['clients.name'];
+    $res->email = $row['clients.email'];
+    $res->tellNumber = $row['clients.tellNumber'];
+    $res->clientId = $row['clients.clientId'];
+    $res->avatarLink = $row['clients.avatarLink'];
+    $res->mailSending = $row['clients.mailSending'];
+
+    array_push($JSONres,$res);
+    }
+    $Res=array('orders'=>$JSONres);
+    echo json_encode($Res);
+?>
---
+++ server/get-all-lessons.php
@@ -0,0 +1,70 @@
+<?php
+    include("connect.php");
+    $id = $_REQUEST['id'];
+
+    $test = $dbh->prepare("SELECT Count(name) FROM admins WHERE email=:id");
+    $test->bindValue('id',$id);
+    $test->execute();
+    
+    if($test->fetch(PDO::FETCH_NUM)[0] > 0)
+    {
+    $projects = $dbh->prepare("SELECT 
+    courses.courseId,
+    courses.name,
+    courses.teacherId,
+    courses.countOfPlace,
+    courses.price,
+    courses.description,
+    courses.duration,
+    style.name,
+    style.id,
+    trainers.name,
+    trainers.email,
+    trainers.tellNumber,
+    trainers.password,
+    trainers.description,
+    trainers.photoLink,
+    lessons.lessonId,
+    lessons.date,
+    rooms.roomId,
+    rooms.roomNumber FROM
+    trainers,courses,styles,lessons,rooms
+    WHERE
+    courses.teacherId = trainers.trainerId and
+    styles.styleId = courses.styleId and
+    courses.courseId = lessons.courseId and
+    lessons.roomID = rooms.roomId
+    ");
+    $projects->execute();
+    $JSONres=array();
+
+    while($row = $projects->fetch(PDO::FETCH_ASSOC)){
+
+    $res = new R();
+    $res->courseId = $row['courses.courseId'];
+    $res->courseName = $row['courses.name'];
+    $res->teacherId = $row['courses.teacherId'];
+    $res->countOfPlace = $row['courses.countOfPlace'];
+    $res->price = $row['courses.price'];
+    $res->courseDescription = $row['courses.description'];
+    $res->duration = $row['courses.duration'];
+    $res->styleId = $row['styles.styleId'];
+    $res->styleName = $row['styles.name'];
+    $res->trainerName = $row['trainers.name'];
+    $res->email = $row['trainers.email'];
+    $res->tellNumber = $row['trainers.tellNumber'];
+    $res->password = $row['trainers.password'];
+    $res->trainerDescription = $row['trainers.description'];
+    $res->photoLink = $row['trainers.photoLink'];
+    $res->lessonId = $row['lessons.lessonId'];
+    $res->date = $row['lessons.date'];
+    $res->roomId = $row['rooms.roomId'];
+    $res->roomNumber = $row['rooms.roomNumber'];
+    
+
+    array_push($JSONres,$res);
+    }
+    $Res=array('allLessons'=>$JSONres);
+    echo json_encode($Res);
+    }
+?>
