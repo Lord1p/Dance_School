497dd3cddbd69a5b192eb5d9f75acd7051be7d9d
Lord1p <olehonator@gmail.com>
2019-05-24 23:48:43 +0300

    Add registration

---
+++ server/post-registration.php
@@ -0,0 +1,45 @@
+<?php
+    include("connect.php");
+
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+
+    $Insert = $dbh->prepare("INSERT INTO
+    clients(name,email,tellNumber,password,mailSending)
+    VALUES(:n,:e,:t,:p,:m)");
+    $Insert->bindValue(':n',$data->name);
+    $Insert->bindValue(':e',$data->email);
+    $Insert->bindValue(':t',$data->tellNumber);
+    $Insert->bindValue(':p',$data->password);
+    $Insert->bindValue(':m',$data->mailSending);
+    $Insert->execute();
+    $clientId = $Insert->insert_id;
+
+    $Select = $dbh->prepare("SELECT
+    name,
+    email,
+    tellNumber,
+    clientId,
+    avatarLink,
+    mailSending
+    FROM
+    clients
+    WHERE
+    clientId = :id
+    ");
+    $Select->bindValue(':id',$clientId);
+    $Select->execute();
+
+    $cl=$Select->fetch(PDO::FETCH_ASSOC)[0];
+
+    $res = new R();
+    $res->name = $cl['clients.name'];
+    $res->email = $cl['clients.email'];
+    $res->tellNumber = $cl['clients.tellNumber'];
+    $res->clientId = $cl['clients.clientId'];
+    $res->avatarLink = $cl['clients.avatarLink'];
+    $res->mailSending = $cl['clients.mailSending'];
+    
+    echo json_encode($res);
+    
+?>
