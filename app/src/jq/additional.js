cd0c5056b4a8ff34174040376b21df6c2a485a62
Andrew Ryban <andrew.ryban@gmail.com>
2019-06-01 17:36:19 +0300

    header size fix

--- app/src/jq/additional.js
+++ app/src/jq/additional.js
@@ -1,7 +1,35 @@
-function hideHeader() {
-    document.getElementById("dance-header").setAttribute("class", "hide");
+function hideByID(id) {
+    if (!id)
+        throw "no Id specified";
+    let element = document.getElementById(id);
+    let classValue = element.attributes.getNamedItem('class').value;
+    let attributes = classValue.split(' ');
+    console.dir(attributes);
+    let i;
+    if (~(i = attributes.indexOf('show'))) {
+        attributes[i] = 'hide';
+    } else {
+        attributes.push('hide');
+    }
+
+    element.setAttribute('class', attributes.join(' '));
 }
 
-function showHeader() {
-    document.getElementById("dance-header").setAttribute("class", "show");
+function showById(id) {
+    if (!id)
+        throw "no Id specified";
+    let element = document.getElementById(id);
+    let classValue = element.attributes.getNamedItem('class').value;
+    let attributes = classValue.split(' ');
+    let i;
+    if (~(i = attributes.indexOf('hide'))) {
+        attributes[i] = 'show';
+    } else {
+        attributes.push('show');
+    }
+
+    element.setAttribute('class', attributes.join(' '));
 }
+
+hideByID('account');
+hideByID('myCourses');
