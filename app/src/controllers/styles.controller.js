b6378b60869f6a3958f15cf17d49d623c02b25f0
Andrew Ryban <andrew.ryban@gmail.com>
2019-06-01 20:02:57 +0300

    styles list update

--- app/src/controllers/styles.controller.js
+++ app/src/controllers/styles.controller.js
@@ -9,6 +9,8 @@
   function StylesController($scope, $http) {
     $scope.styles = [];
     $scope.empty = "А стилей, то нет";
+    var counter = 0;
+    var maxCounter = 7;
     init();
 
     function init() {
@@ -16,7 +18,25 @@
         .then(res => {
           console.log(res.data.styles);
           $scope.styles = res.data.styles;
+          for (let i of $scope.styles) {
+            i.color = generateColor();
+          }
         });
+    }
+
+    function generateColor() {
+      ++counter;
+      if (counter == 8) counter = 1;
+
+      switch (counter) {
+        case 1: return 'bg-primary';
+        case 2: return 'bg-secondary';
+        case 3: return 'bg-success';
+        case 4: return 'bg-danger';
+        case 5: return 'bg-warning';
+        case 6: return 'bg-info';
+        case 7: return 'bg-dark';
+      }
     }
   }
 })();
